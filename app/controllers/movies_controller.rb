75398c64c0d7bc4b3479a371f827cbf6b8f3fd1d
neil patel <neil@myindianweddingapp.com>
2015-11-07 14:31:24 +0000

    added associations

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,36 +4,51 @@ class MoviesController < ApplicationController
   respond_to :html
 
   def index
-    @movies = Movie.all
-    respond_with(@movies)
+    @movies = current_user.movies
   end
 
   def show
-    respond_with(@movie)
   end
 
   def new
     @movie = Movie.new
-    respond_with(@movie)
   end
 
   def edit
   end
 
   def create
-    @movie = Movie.new(movie_params)
-    @movie.save
-    respond_with(@movie)
+    @movie = current_user.movies.new(movie_params)
+
+    respond_to do |format|
+    if @movie.save
+        format.html { redirect_to @movie, notice: 'Movie was successfully created.' }
+        format.json { render :show, status: :created, location: @movie }
+      else
+        format.html { render :new }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
-    @movie.update(movie_params)
-    respond_with(@movie)
+    respond_to do |format|
+   if @movie.update(movie_params)
+        format.html { redirect_to @movie, notice: 'Movie was successfully updated.' }
+        format.json { render :show, status: :ok, location: @movie }
+      else
+        format.html { render :edit }
+        format.json { render json: @movie.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
     @movie.destroy
-    respond_with(@movie)
+    respond_to do |format|
+    format.html { redirect_to movies_url, notice: 'Movie was successfully destroyed.' }
+    format.json { head :no_content }
+    end
   end
 
   private
