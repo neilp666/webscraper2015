ebc2d36a6f9d4b5fbce72de93a6157a1838738d7
neil patel <neil@myindianweddingapp.com>
2015-11-10 19:16:13 +0000

    fixed synopsis field

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -2,9 +2,10 @@ class Scrape
   attr_accessor :title, :hotness, :image_url, :rating, :director,
   :genre, :release_date, :runtime, :synopsis, :failure
 
-  def scrape_new_movie
+  def scrape_new_movie(url)
     begin
-      doc = Nokogiri::HTML(open("http://www.rottentomatoes.com/m/the_martian/"))
+      doc = Nokogiri::HTML(open(url))
+
       doc.css('script').remove 
       self.title = doc.at("//span[@itemprop = 'name']").text  
       self.hotness = doc.at("//span[@itemprop = 'ratingValue']").text.to_i 
@@ -14,29 +15,10 @@ class Scrape
       self.genre = doc.at("//span[@itemprop = 'genre']").text 
       self.release_date = doc.at("//td[@itemprop = 'datePublished']").text.to_date  
       self.runtime = doc.at("//time[@itemprop = 'duration']").text 
-      s = doc.css('#movieSynopsis').text 
-      if ! s.valid_encoding?
-        s = s.encode("UTF-16be", :invalid=> :replace, :replace=>"?").encode('UTF-8')
-      end
+      self.synopsis = doc.css("#movieSynopsis").text.tidy_bytes
       return true
     rescue Exception => e
       self.failure = "Something went wrong with the scrape"
     end
-  end
-
-
-    def save_movie
-    movie = Movie.new(
-      title: self.title,
-      hotness: self.hotness,
-      image_url: self.image_url,
-      synopsis: self.synopsis,
-      rating: self.rating,
-      genre: self.genre,
-      director: self.director,
-      release_date: self.release_date,
-      runtime: self.runtime
-      )
-    movie.save
   end
 end
